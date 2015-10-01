34c65360d1361ca40606689b4d314a04b3f96efc
jaroso78 <jaroso78@yahoo.es>
2015-10-01 11:50:27 +0200

    Start repository

---
+++ holamundo/index.php
@@ -0,0 +1,25 @@
+<html>
+<html>
+
+	<head>
+		<title>
+			Mi primera página web.
+		</title>
+	</head>
+
+	<body>
+		Mi primer código html.
+		<br>
+		<?php
+			echo"hola mundo";
+			echo "<br>";
+			echo "Esto abarca
+			 	  múltiples lineas. los saltos de línea también 
+			      se mostrarán.";
+
+		?>
+	</body>
+
+
+
+</html>
---
+++ probandoXDebug/index.php
@@ -0,0 +1,27 @@
+<html>
+<html>
+
+	<head>
+		<title>
+			Mi primera página web.
+		</title>
+	</head>
+
+	<body>
+		Mi primer código html.
+		<br>
+		<?php
+			echo"hola mundo";
+			echo "<br>";
+			echo "Esto abarca
+			 	  múltiples lineas. los saltos de línea también 
+			      se mostrarán.";
+			 $numero=23;
+			 echo $numero;
+
+		?>
+	</body>
+
+
+
+</html>
